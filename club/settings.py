976e524b3e71af8aec31a52889506fdb9a91244e
cartmendum <maxim_dorofeev@mail.ru>
2021-12-30 06:24:27 -0500

    Чиню багу с выкаткой

--- club/settings.py
+++ club/settings.py
@@ -163,7 +163,7 @@ EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Вастрик.Клуб <mnogosdelal.club@gmail.com>")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Джедайский Клуб <mnogosdelal.club@gmail.com>")
 
 # App
 
