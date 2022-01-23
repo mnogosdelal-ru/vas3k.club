654c46a5cf9974e7d7118978062d24b2dc01937e
Mikhail <glader.ru@gmail.com>
2022-01-23 21:25:51 +0300

    Чеки

--- payments/cloudpayments.py
+++ payments/cloudpayments.py
@@ -1,4 +1,4 @@
-# https://developers.cloudpayments.ru/#api
+# https:# developers.cloudpayments.ru/#api
 
 import base64
 import hashlib
@@ -113,12 +113,38 @@ class CloudPaymentsService:
             "Description": product_data["description"],
             "RequireConfirmation": False,
             "InvoiceId": order_id,
-            "SuccessRedirectUrl": "https://club.mnogosdelal.ru/intro/",
+            "SuccessRedirectUrl": "https:# club.mnogosdelal.ru/intro/",
             "Email": user.email,
+            "JsonData": {
+                "CloudPayments": {
+                    "CustomerReceipt": {
+                        "Items": [
+                            {
+                                "label": product_data["description"],
+                                "price": product_data["amount"],
+                                "quantity": 1.00,
+                                "amount": product_data["amount"],
+                                "vat": 0,
+                                "method": 0,
+                                "object": 0,
+                                "measurementUnit": "шт",
+                            },
+                        ],
+                        "email": user.email,
+                        "amounts":
+                        {
+                            "electronic": product_data["amount"],
+                            "advancePayment": 0,
+                            "credit": 0,
+                            "provision": 0,
+                        },
+                    },
+                },
+            },
         }
 
         response = requests.post(
-            "https://api.cloudpayments.ru/orders/create",
+            "https:# api.cloudpayments.ru/orders/create",
             auth=HTTPBasicAuth(settings.CLOUDPAYMENTS_API_ID, settings.CLOUDPAYMENTS_API_PASSWORD),
             data=payload,
         )
