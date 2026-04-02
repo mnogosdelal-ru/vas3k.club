0a43d7842119a7416f4de8b9460e6a929f1f9c3d
Mikhail Polykovskii <glader.ru@gmail.com>
2026-04-02 13:57:56 +0300

    Переключение на bot polling в продакшене

--- bot/main.py
+++ bot/main.py
@@ -95,18 +95,10 @@ def main() -> None:
     dispatcher.add_handler(MessageHandler(Filters.forwarded & Filters.private, whois.command_whois))
     dispatcher.add_handler(MessageHandler(Filters.private, private_message))
 
-    # Start the bot
-    if settings.DEBUG:
-        updater.start_polling()
-        # ^ polling is useful for development since you don't need to expose webhook endpoints
-    else:
-        updater.start_webhook(
-            listen=settings.TELEGRAM_BOT_WEBHOOK_HOST,
-            port=settings.TELEGRAM_BOT_WEBHOOK_PORT,
-            url_path=settings.TELEGRAM_TOKEN,
-        )
-        log.info(f"Set webhook: {settings.TELEGRAM_BOT_WEBHOOK_URL + settings.TELEGRAM_TOKEN}")
-        updater.bot.set_webhook(settings.TELEGRAM_BOT_WEBHOOK_URL + settings.TELEGRAM_TOKEN)
+    # Start the bot (polling mode — Telegram servers can't reach this host directly)
+    log.info("Starting bot in polling mode")
+    updater.bot.delete_webhook()
+    updater.start_polling()
 
     # Wait all threads
     updater.idle()
